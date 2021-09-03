ae79bedea811d88a3c737ae6ef5c5602e2e25f84
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-09-02 17:27:38 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
+if [[ ! -f os.iso ]]; then
+wget https://builds.vinix-os.org/repos/files/vinix/latest/vinix.iso
+mv vinix.iso os.iso
+fi
 qemu-system-x86_64 -cpu qemu64,level=11,+rdtscp -M q35,smm=off -m 2G -smp 4 -cdrom os.iso -serial stdio
