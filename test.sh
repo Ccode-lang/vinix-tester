fcb625639da9781deecfc2b4b2d3ba16aaa60462
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-09-27 09:20:34 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,6 @@ if [[ ! -f os.iso ]]; then
 wget https://builds.vinix-os.org/repos/files/vinix/latest/vinix.iso
 mv vinix.iso os.iso
 fi
-qemu-system-x86_64 -cpu qemu64,level=11,+rdtscp -M q35,smm=off -m 2G -smp 4 -cdrom os.iso -serial stdio
+qemu-system-x86_64 -cpu qemu64,level=11,+rdtscp -M q35,smm=off -m 8G -smp 4 -cdrom os.iso -serial stdio -no-shutdown -no-reboot
+#qemu-system-x86_64 -cpu qemu64,level=11,+rdtscp -M q35,smm=off -m 8G -smp 4 -cdrom os.iso -serial stdio -no-shutdown -no-reboot -d int
+# add the line before if you want a lot of terminal spam.
