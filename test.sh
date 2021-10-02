7db3c035b22c85cc2ac228163e6ce3ed1fd4ceec
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-10-01 18:35:14 -0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ mv vinix.iso os.iso
 fi
 qemu-system-x86_64 -cpu qemu64,level=11,+rdtscp -M q35,smm=off -m 8G -smp 4 -cdrom os.iso -serial stdio -no-shutdown -no-reboot
 #qemu-system-x86_64 -cpu qemu64,level=11,+rdtscp -M q35,smm=off -m 8G -smp 4 -cdrom os.iso -serial stdio -no-shutdown -no-reboot -d int
-# add the line before if you want a lot of terminal spam.
+# comment other line and add the line before if you want a lot of terminal spam.
